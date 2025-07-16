5e19151ecec1b31946fbb7b736d0e11cb8b441b9
Aryanvaniya007 <23cp047@bvmengineering.ac.in>
2025-07-16 21:07:08 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (75.06%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int k = 0; // Pointer for the next position of a valid (not equal to val) number
+        int k = 0; 
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != val) {
-                nums[k] = nums[i]; // Move the valid number to the front
+                nums[k] = nums[i]; 
                 k++;
             }
         }
