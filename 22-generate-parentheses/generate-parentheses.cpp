8eb6d028419959d867a4c27e766ae202789119ba
Aryanvaniya007 <23cp047@bvmengineering.ac.in>
2025-07-29 22:15:54 +0530

    Time: 3 ms (71.96%) | Memory: 16.4 MB (14.42%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    std::vector<std::string> generateParenthesis(int n) {
+        std::vector<std::string> result;
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+
+private:
+    void backtrack(std::vector<std::string>& result, std::string current_string, int open_count, int close_count, int n) {
+        if (current_string.length() == 2 * n) {
+            result.push_back(current_string);
+            return;
+        }
+
+        if (open_count < n) {
+            current_string.push_back('(');
+            backtrack(result, current_string, open_count + 1, close_count, n);
+            current_string.pop_back();
+        }
+
+        if (close_count < open_count) {
+            current_string.push_back(')');
+            backtrack(result, current_string, open_count, close_count + 1, n);
+            current_string.pop_back();
+        }
+    }
+};
