4c1fbe8f331b990eac99cc55f60672528548e950
Aryanvaniya007 <23cp047@bvmengineering.ac.in>
2026-03-01 23:39:55 +0530

    Time: 3 ms (29.65%) | Memory: 95 MB (80.48%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        // Ensure nums1 is the smaller array
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        
+        int m = nums1.size();
+        int n = nums2.size();
+        int imin = 0, imax = m, halfLen = (m + n + 1) / 2;
+        
+        while (imin <= imax) {
+            int i = (imin + imax) / 2;
+            int j = halfLen - i;
+            
+            if (i < m && nums2[j - 1] > nums1[i]) {
+                // i is too small, must increase it
+                imin = i + 1;
+            } else if (i > 0 && nums1[i - 1] > nums2[j]) {
+                // i is too big, must decrease it
+                imax = i - 1;
+            } else {
+                // i is perfect
+                int maxLeft = 0;
+                if (i == 0) maxLeft = nums2[j - 1];
+                else if (j == 0) maxLeft = nums1[i - 1];
+                else maxLeft = max(nums1[i - 1], nums2[j - 1]);
+                
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+                
+                int minRight = 0;
+                if (i == m) minRight = nums2[j];
+                else if (j == n) minRight = nums1[i];
+                else minRight = min(nums1[i], nums2[j]);
+                
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        
+        return 0.0; // Should never reach here
+    }
+};
