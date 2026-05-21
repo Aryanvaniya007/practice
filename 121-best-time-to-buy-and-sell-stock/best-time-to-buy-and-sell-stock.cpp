e273904f354175d595dac22875733ffdaf59c1c3
Vaniya Aryan Hareshbhai <23cp047@bvmengineering.ac.in>
2026-05-21 21:41:42 +0530

    Time: 3 ms (28.73%) | Memory: 97.5 MB (6.83%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_price = prices[0];
+        int profit = 0;
+        for(int i=0;i<prices.size();i++)
+        {
+            int cost = prices[i] - min_price;
+            profit = max(profit,cost);
+            min_price = min(min_price,prices[i]);
+        }
+        return profit;
+    }
+};
